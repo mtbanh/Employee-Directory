2a732600cc3808e5019b2f5c86f59ca8c9550aab
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-12 21:16:52 -0700

    created Header

---
+++ emp-dir/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+
+const Header = ()=>{
+    return(
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Search for employees using the search box or sort employees by clicking on each heading</p>
+        </div>
+    )
+}
+
+export default Header;
--- emp-dir/src/components/NavBar/NavBar.js
+++ emp-dir/src/components/NavBar/NavBar.js
@@ -1,1 +1,9 @@
 import React from "react";
+// import SearchName from "../SearchName";
+import "./navbar.css";
+
+const Nav = ()=>{
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light"
+    )
+}
