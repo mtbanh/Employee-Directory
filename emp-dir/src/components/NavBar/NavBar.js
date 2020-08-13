b4617eec0edecd4bbc6eacf79068760f03b267da
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-12 22:33:06 -0700

    created navbar with searchbox

--- emp-dir/src/components/NavBar/NavBar.js
+++ emp-dir/src/components/NavBar/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
-// import SearchName from "../SearchName";
+import SearchName from "../SearchName";
 import "./navbar.css";
 
 const Nav = ()=>{
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="search-area col-4">
+                <SearchName />
+            </div>
+        </nav>
     )
 }
+
+export default Nav;
